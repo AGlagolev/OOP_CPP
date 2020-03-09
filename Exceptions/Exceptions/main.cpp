7da0cbdf1b49578a38bce6f0a90e4ad26a90dcbd
Alex <Alex@DESKTOP-CEQG6CC>
2020-03-09 13:06:01 +0200

    df

--- Exceptions/Exceptions/main.cpp
+++ Exceptions/Exceptions/main.cpp
@@ -1,11 +1,32 @@
 #include<iostream>
 
+//#define DIV_BY_ZERP
+#define ARIPHM_OVERFLOW
+
+int factorial(int n)
+{
+	unsigned int f = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		
+			if (n > 12) throw std::exception("Error: Ariphemetical overflow");
+			//std::cout << i << "! = ";
+			f *= i;
+			//std::cout << f << std::endl;
+		
+		
+	}
+	//std::cout << n << "! = " << f << std::endl;
+	return f;
+}
+
 void main()
 {
 	setlocale(LC_ALL, "");
 	system("color 0A");
-	int a,b;
-	
+#ifdef DIV_BY_ZERP
+	int a, b;
+
 	//
 	//try
 	//{
@@ -21,7 +42,7 @@ void main()
 		std::cout << "Input two numbers: "; std::cin >> a >> b;
 		system("cls");
 		if (b == 0) throw std::exception("Деление на 0 ");
-		std::cout << a/ b;
+		std::cout << a / b;
 	}
 	catch (std::exception&e)
 	{
@@ -31,4 +52,20 @@ void main()
 	{
 		std::cerr << "Something happend" << std::endl;
 	}
+#endif // DIV_BY_ZERP
+
+#ifdef ARIPHM_OVERFLOW
+	int n;
+	
+	std::cout << "Input number: "; std::cin >> n;
+	try
+	{
+		std::cout << n << "! = " << factorial(n) << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+#endif // ARIPHM_OVERFLOW
+
 }
